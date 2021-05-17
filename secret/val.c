63a289924fe972524920b84f827d602186cb4d9c
Sholde <exshode@gmail.com>
2021-05-17 19:48:16 +0200

    adding others tool for valgrind

--- secret/val.c
+++ secret/val.c
@@ -1,7 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
+
+int func(int *a)
+{
+  a[5] = 1;
+
+  int b;
+  if (b)
+    printf("lol\n");
+}
 
 int main()
 {
   int *a = malloc(sizeof(int) * 5);
+  func(a);
   return 0;
 }
